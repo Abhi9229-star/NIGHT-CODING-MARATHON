e1ed5fda348f120f3d9e50a01f466b093c13c5df
Abhishek yadav <ak9608122@gmail.com>
2026-04-06 17:18:17 +0530

    commitagains

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  `${import.meta.env.VITE_API_BASE_URL || "http://localhost:9000"}/api`;
+  `${import.meta.env.VITE_API_BASE_URL || "https://night-coding-marathon-hgsc.vercel.app/"}/api`;
 
 export const API_PATHS = {
   AUTH: {
